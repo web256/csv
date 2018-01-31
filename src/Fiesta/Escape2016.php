574dd0647cd2c106e009cba3c973fb971330344e
web256 <wangdk369@gmail.com>
2018-01-31 16:38:17 +0800

    add csv_class.php

--- src/Fiesta/Escape2016.php
+++
@@ -1,9 +0,0 @@
-namespace Ford\Escape;
-
-class Escape2016
-{
-    public static function info()
-    {
-        echo "This is Ford Escape2016!<br />";
-    }
-}
---
+++ test/demo.php
@@ -0,0 +1,5 @@
+<?php
+require './vendor/autoload.php';
+use Fiesta\Fusion2016;
+$car = new Fusion2016();
+$car->info();
